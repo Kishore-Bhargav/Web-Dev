ae36d611ce84e26269c55618e3940957ef70202b
Kishore-Bhargav <138761709+Kishore-Bhargav@users.noreply.github.com>
2024-05-24 19:21:26 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
-Services();
-
-function Services(){
-    document.getElementsByClassName("display").textContent="Services";
+function services(){
+    document.getElementById("display").textContent="Services";
 }
 
+function projects(){
+    document.getElementById("display").textContent="Projects";
+}
 
+function about(){
+    document.getElementById("display").textContent="About";
+}
+
+function contact(){
+    document.getElementById("display").textContent="Contact";
+}
