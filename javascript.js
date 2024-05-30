055480480d878e200acbbf7961f8c2fae1da77e3
Kishore-Bhargav <138761709+Kishore-Bhargav@users.noreply.github.com>
2024-05-31 00:42:21 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -19,3 +19,14 @@ function Form(){
     form.style.display="inline-block";
 }
 
+function Details(){
+    let display2=document.getElementById("display2");
+    let u_name=document.getElementById("username").value;
+    let u_mail=document.getElementById("email").value;
+    let u_pass=document.getElementById("password").value;
+    document.getElementById("uname").textContent= u_name;
+    document.getElementById("umail").textContent= u_mail;
+    document.getElementById("upass").textContent= u_pass;
+    display2.style.display="inline-block";
+}
+
